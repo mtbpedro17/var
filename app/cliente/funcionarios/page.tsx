9d088b04a087dd73bd09fce46aa7750cd82ba2b1
mtbpedro17 <mtbpedro17@gmail.com>
2026-02-27 17:30:04 +0100

    ultimas alterções

--- app/cliente/funcionarios/page.tsx
+++ app/cliente/funcionarios/page.tsx
@@ -1,49 +1,8 @@
-import UltimosAlertas from "@/components/alerta";
 import Caixa5 from "@/components/caixa5";
 import Container from "@/components/container";
 import Sidebar2 from "@/components/sidbar2";
-import Tabela5, { Atividade } from "@/components/tabela5";
-import { Bell, Calendar,  Layers,  UserCheck,  Users2, UserX } from "lucide-react";
-const dadosAtividades: Atividade[] = [
-  {
-    funcionario: "Estefânio Schofield",
-    status: "Ativo",
-    ultimoAcesso: "14:45"
-  },
-  {
-    funcionario: "Josimar Pedro",
-    status: "Ativo",
-    ultimoAcesso: "14:45 "
-  },
-  {
-    funcionario: "Samuel Josué",
-    status: "Inativo",
-    ultimoAcesso: "14:45"
-  }
-];
+import { Bell, UserCheck,  Users2, UserX } from "lucide-react";
 
-const alertasExemplo = [
-    {
-        titulo: "Nova atualização disponível",
-        data: "Hoje, 19:40"
-    },
-    {
-        titulo: "Erro de conexão com App Mobile",
-        data: "Ontem, 08:20"
-    },
-    {
-        titulo: "Tentativa de acesso não autorizado",
-        data: "Hoje, 15:55"
-    },
-    {
-        titulo: "Erro de conexão com App Mobile",
-        data: "Ontem, 08:20"
-    },
-    {
-        titulo: "Tentativa de acesso não autorizado",
-        data: "Ontem, 17:28"
-    }
-];
 export default function dashboard() {
     return(
         <div>
@@ -70,3 +29,10 @@ export default function dashboard() {
         </div>
     )
 }
+
+{/* 
+    git add .
+    git commit -m "descrição das alterações"
+    git push origin main
+    
+    */}
